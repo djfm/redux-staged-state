4e211b9ebdc03c0ae080bcf86dad834688cb1aa2
djfm <fm.de.jouvencel@gmail.com>
2016-03-22 21:59:15 +0100

    The reducer responds to delete actions.

--- src/reducer.js
+++ src/reducer.js
@@ -1,9 +1,13 @@
-import { SET_TYPE } from './constants';
+import { SET_TYPE, DELETE_TYPE } from './constants';
 import { deserialize as deserializeAccessor } from './accessors';
 
 export const reducer = (state = {}, action) => {
   if (action.type === SET_TYPE) {
     return deserializeAccessor(action.accessor).of(state).set(action.value);
+  }
+
+  if (action.type === DELETE_TYPE) {
+    return deserializeAccessor(action.accessor).of(state).delete();
   }
 
   return state;
--- test/reducer.js
+++ test/reducer.js
@@ -2,7 +2,7 @@
 import deepFreeze from 'deep-freeze';
 
 import { reducer } from '../src/reducer';
-import { SET_TYPE } from '../src/constants';
+import { SET_TYPE, DELETE_TYPE } from '../src/constants';
 
 describe('The reducer', () => {
   it('should set values in the staged state', () =>
@@ -11,5 +11,11 @@ describe('The reducer', () => {
       accessor: 'a',
       value: 1,
     }).should.deep.equal({ a: 1 })
+  );
+  it('should delete values in the staged state', () =>
+    reducer(deepFreeze({ a: 1 }), {
+      type: DELETE_TYPE,
+      accessor: 'a',
+    }).should.deep.equal({})
   );
 });
