a90f4ecc2f2cc174e67be385a2de884b82a31b96
djfm <fm.de.jouvencel@gmail.com>
2016-03-24 17:37:44 +0100

    // test robustness attempt #2

--- test/zzz-dist.js
+++ test/zzz-dist.js
@@ -1,7 +1,7 @@
 /* global describe, it */
 
 describe('The built library is a commonjs2 module', function checkBuildArtefacts() {
-  this.timeout(5000);
+  this.timeout(15000);
   it('should expose "stage", "connectStaged", and "reducer"', () => {
     require('..').should.have.keys('stage', 'connectStaged', 'reducer');
   });
