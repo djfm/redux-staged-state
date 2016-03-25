c4b46694289b370758370ae7695065460196a62a
djfm <fm.de.jouvencel@gmail.com>
2016-03-25 19:35:02 +0100

    expose prop and composeAccessors too

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export { reducer } from './reducer';
 export { connectStaged } from './react-bridge';
 export { stage } from './stage';
+export { prop, compose as composeAccessors } from './accessors';
